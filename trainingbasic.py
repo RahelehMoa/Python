46bba816770e53031706c54850df2f31b0d39adf
Raheleh Moallemi <87756714+RahelehMoa@users.noreply.github.com>
2026-06-10 18:56:30 +0330

    Add files via upload

--- trainingbasic.py
+++ trainingbasic.py
@@ -173,3 +173,137 @@ print("table" in Italian_English.values())
 print(2 in Italian_English.keys())
 print("patata" in Italian_English)
 
+#--------------------------------------------------------------------------------------------------
+#-----------------------------------------SET------------------------------------------------------
+my_set = {1.0,"Hello",(1,2,3)}
+print(my_set)
+print(type(my_set))
+
+#my_set[0]    #ERROR
+print(1.0 in my_set)
+
+my_set.add("nuovo")
+my_set.update(["primo", "secondo"])
+print(my_set)
+
+my_set.discard((1,2,3))
+print(my_set)
+
+a = {1, 2, 3, 4, 5}
+b = {4, 5, 6, 7, 8}
+#print(a + b)  #UNSUPPORTED
+print(a - b)   
+#print(a * 2)  #UNSUPORTED
+
+x = a.union(b) # (a | b) unione
+print(x)
+
+y = a.intersection(b) # (a & b) intersezione
+print(y)
+
+w = a.symmetric_difference(b) # (a ^ b) diff simmetrica
+print(w)
+
+z = a.difference(b) # (a - b) differenza
+print(z)
+
+#--------------------------------------------------------------------------------------------------
+#---------------------------------Generic operators------------------------------------------------
+
+l = [1, 3, 5]
+print(sum(l))
+print(min(l))
+print(max(l))
+print(min(3, 5))
+#sum(3, 5) #ERROR
+
+pi =-3.14159
+print(abs(pi))
+print(round(pi))
+print(round(pi, 3))
+
+sentences = "esempio stringa"
+list = [1, 3, 5]
+my_dict = {"cane":"dog", "gatto":"cat"}
+print(len(sentences))
+print(len(list))
+print(len(my_dict))
+
+#--------------------------------------------------------------------------------------------------
+#---------------------------------------------Del--------------------------------------------------
+New_list = [1, 4, 7]
+del New_list
+#print(New_list)  #ERROR New_list is empty
+
+New_list2 = [1, 4, 7]
+New_dict3 = {"cane":"dog", "gatto":"cat"}
+
+del New_list2[1]
+del New_dict3["cane"]
+
+print(New_list2)
+print(New_dict3)
+
+#--------------------------------------------------------------------------------------------------
+#---------------------------------------------Dir--------------------------------------------------
+
+print(dir(New_list2))
+print(dir(str))
+print(dir("papera"))
+print("-------------------------------------------------------------------------------------------")
+#--------------------------------------------------------------------------------------------------
+#--------------------------------------------Date Time---------------------------------------------
+
+import datetime as dt
+
+d = dt.date(2026, 6, 20)
+print(d)
+print(type(d))
+
+dir(dt.date)
+
+today = dt.date.today()
+print("Year Current:", today.year)
+print("Month Current:", today.month)
+print("Day Current:", today.day)
+
+print("-------------------------------------------------------------------------------------------")
+
+Hour = dt.time(14, 55, 27)
+print(Hour)
+print(type(Hour))
+
+orario = dt.time(14, 55, 27)
+print("Hour Current:", orario.hour)
+print("Minute Current:", orario.minute)
+print("Second Current:", orario.second)
+
+print("-------------------------------------------------------------------------------------------")
+
+d1 = dt.datetime(2026, 7, 15)
+print(d1)
+print(type(d1))
+
+d2 = dt.datetime(2026,7,15, 14,55,27)
+print(d2)
+print(type(d2))
+
+print(dt.datetime.now())
+print(d2.time())
+print(d2.date())
+
+print("-------------------------------------------------------------------------------------")
+
+print("Year Corrent:", d2.year)
+print("Month Corrent:", d2.month)
+print("Day Corrent:", d2.day)
+print("Hour Corrent:", d2.hour)
+print("Minuti Corrent:", d2.minute)
+print("Second Corrent:", d2.second)
+
+print("---------------------------------------------------------------------------------------")
+
+
+
+
+
